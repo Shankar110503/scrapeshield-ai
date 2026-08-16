8499f6901ace0b0d7dcbd1cac44139265e9e0cb0
Shankar lal Godara <godarashankar958@gmail.com>
2026-08-16 22:47:46 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class FieldSpec:
+    name: str
+    description: str
+
+@dataclass
+class HealthReport:
+    healthy: bool
+    missing_fields: List[str]
+    row_count: int
+    message: str
